7a5c012fed50ae10f9eec8cde53d442df5b248b8
xiuying545 <xiuying545@gmail.com>
2026-01-25 22:53:52 +0800

    Update workflow

--- Assessment-main/Assessment-Backend/src/test/java/com/assessment/AssessmentApplicationTests.java
+++ Assessment-main/Assessment-Backend/src/test/java/com/assessment/AssessmentApplicationTests.java
@@ -3,9 +3,17 @@ package com.assessment;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(properties = {
+		"spring.datasource.url=jdbc:h2:mem:testdb", // in-memory DB
+		"spring.datasource.driverClassName=org.h2.Driver",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class AssessmentApplicationTests {
 
 	@Test
